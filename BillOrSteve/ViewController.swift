5467efbb0c8684b645cfc708612647b66a78c4a4
briecoyle <brie.coyle@gmail.com>
2016-10-14 12:17:47 -0400

    Complete Bill & Steve

--- BillOrSteve/ViewController.swift
+++ BillOrSteve/ViewController.swift
@@ -11,15 +11,75 @@ import UIKit
 class ViewController: UIViewController {
     
     // Create your stored properties here
+    var billAndSteveFacts: [String: [String]] = [:]
+    var correctPerson: String = ""
+    var score: Int = 0
     
-    
+    @IBOutlet weak var factLabel: UILabel!
+    @IBOutlet weak var scoreCounter: UILabel!
+    @IBOutlet weak var steveButton: UIButton!
+    @IBOutlet weak var billButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        createFacts()
     }
     
+    func createFacts() {
+        let billFacts = [
+        "He aimed to become a millionaire by the age of 30. However, he became a billionaire at 31.",
+        "He scored 1590 (out of 1600) on his SATs.",
+        "His foundation spends more on global health each year than the United Nation's World Health Organization.",
+        "The private school he attended as a child was one of the only schools in the US with a computer. The first program he ever used was a tic-tac-toe game.",
+        "In 1994, he was asked by a TV interviewer if he could jump over a chair from a standing position. He promptly took the challenge and leapt over the chair like a boss."
+        ]
+        let steveFacts = [
+        "He took a calligraphy course, which he says was instrumental in the future company products' attention to typography and font.",
+        "Shortly after being shooed out of his company, he applied to fly on the Space Shuttle as a civilian astronaut (he was rejected) and even considered starting a computer company in the Soviet Union.",
+        "He actually served as a mentor for Google founders Sergey Brin and Larry Page, even sharing some of his advisers with the Google duo.",
+        "He was a pescetarian, meaning he ate no meat except for fish."
+        ]
+        billAndSteveFacts["Bill Gates"] = billFacts
+        billAndSteveFacts["Steve Jobs"] = steveFacts
+    }
     
+    func getRandomFact() -> (String, String) {
+        let person = randomPerson()
+        var personsRandomFact: String = ""
+        if let personsFacts = billAndSteveFacts[person] {
+            let randomFact = randomIndex(fromArray: personsFacts)
+            personsRandomFact = personsFacts[randomFact]
+            
+        }
+        return (person, personsRandomFact)
+    }
     
+    func showFact() {
+        
+        var correctFact: String = ""
+        let personAndFact = getRandomFact()
+        correctPerson = personAndFact.0
+        correctFact = personAndFact.1
+        factLabel.text = correctFact
+    }
     
+    @IBAction func stevePressed(_ sender: AnyObject) {
+        if correctPerson == "Steve Jobs" {
+            score += 1
+            scoreCounter.text = "\(score) out of 9"
+            showFact()
+        } else {
+            showFact()
+        }
+    }
+    @IBAction func billPressed(_ sender: AnyObject) {
+        if correctPerson == "Bill Gates" {
+            score += 1
+            scoreCounter.text = "\(score) out of 9"
+            showFact()
+        } else {
+            showFact()
+        }
+    }
     
     // Helper Functions
     func randomIndex(fromArray array: [String]) -> Int {
