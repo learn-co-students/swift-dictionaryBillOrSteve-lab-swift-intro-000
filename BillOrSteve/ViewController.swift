415cf34cc941369b631567d99f88ee7c3501f48e
jajuki13 <khkim1776@gmail.com>
2016-10-31 21:10:08 -0400

    created funcs and dict with facts

--- BillOrSteve/ViewController.swift
+++ BillOrSteve/ViewController.swift
@@ -9,19 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var randomFact: UILabel!
+    @IBOutlet weak var steveJobsPortrait: UIButton!
+    @IBOutlet weak var billGatesPortrait: UIButton!
+    @IBOutlet weak var correctCounter: UILabel!
+    @IBOutlet weak var wrongCounter: UILabel!
     
     // Create your stored properties here
-    
+    var billAndSteveFacts: [String : [String]] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
-    
-    
-    
     // Helper Functions
+    func createFacts() {
+        let steveFacts = [
+            "He took a calligraphy course, which he says was instrumental in the future company products' attention to typography and font.",
+            "Shortly after being shoed out of his company, he applied to fly on the Space Shuttle as a civilian astronaut (he was rejected) and even considered starting a computer company in the Soviet Union.",
+            "He actually served as a mentor for Google founders Sergey Brin and Larry Page, even sharing some of his advisers with the Google duo.",
+            "He was a pescetarian, meaning he at no meat except for fish."
+        ]
+        
+        let billFacts = [
+            "He aimed to become a millionaire by the age of 30. However, he became a billionaire at 31.",
+            "He scored 1590 (out of 1600) on his SATs.",
+            "His foundation spends more on global health each year than the United Nation's World Health Organization.",
+            "The private school he attended as a child was one of the only schools in the US with a computer. The first program he ever used was a tic-tac-toe game.",
+            "In 1994, he was asked by a TV interviewer if he could jump over a chair from a standing position. He promptly took the challenge and leapt over the chair like a boss."
+        ]
+        
+        billAndSteveFacts["Bill Gates"] = billFacts
+        billAndSteveFacts["Steve Jobs"] = steveFacts
+    }
+    
+    func getRandomFact() -> (String, String) {
+        //todo
+        // pulls a random fact
+        return (String, String)
+    }
+    
+    func showFact() {
+        //todo
+        // upon click shows the fact.
+        // updates the counter if it was wrong
+    }
+    
     func randomIndex(fromArray array: [String]) -> Int {
         return Int(arc4random_uniform(UInt32(array.count)))
     }
@@ -35,5 +68,4 @@ class ViewController: UIViewController {
             return "Bill Gates"
         }
     }
-    
 }
