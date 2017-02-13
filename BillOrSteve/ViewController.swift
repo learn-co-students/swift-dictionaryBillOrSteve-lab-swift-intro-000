fc28674b07d4bd21f796b32c27a195de5fb5a1ca
larenSakota <jedi.temple@yahoo.com>
2017-02-13 12:21:30 -0800

    a little stuck

--- BillOrSteve/ViewController.swift
+++ BillOrSteve/ViewController.swift
@@ -10,14 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var factLabel: UILabel!
+    @IBOutlet weak var factCounter: UILabel!
+    @IBOutlet weak var steveLabel: UILabel!
+    @IBOutlet weak var billLabel: UILabel!
+    
+    @IBOutlet weak var steveBtn: UIButton!
+    @IBOutlet weak var billBtn: UIButton!
+    
+    
     // Create your stored properties here
+    var billAndSteveFacts: [String : [String]] = [:]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
     }
     
-    
+    func createFacts() {
+        let billFacts =  ["He aimed to become a millionaire by the age of 30. However, he became a billionaire at 31.",
+                          "He scored 1590 (out of 1600) on his SATs.",
+                          "His foundation spends more on global health each year than the United Nation's World Health Organization.",
+                          "The private school he attended as a child was one of the only schools in the US with a computer. The first program he ever used was a tic-tac-toe game.",
+                          "In 1994, he was asked by a TV interviewer if he could jump over a chair from a standing position. He promptly took the challenge and leapt over the chair like a boss."
+                            ]
+        let steveFacts = ["He took a calligraphy course, which he says was instrumental in the future company products' attention to typography and font.",
+                          "Shortly after being shooed out of his company, he applied to fly on the Space Shuttle as a civilian astronaut (he was rejected) and even considered starting a computer company in the Soviet Union.",
+                          "He actually served as a mentor for Google founders Sergey Brin and Larry Page, even sharing some of his advisers with the Google duo.",
+                          "He was a pescetarian, meaning he ate no meat except for fish."
+                            ]
+        
+        billAndSteveFacts["Bill Gates"] = billFacts
+        billAndSteveFacts["Steve Jobs"] = steveFacts
+    }
     
     
     
@@ -34,6 +61,54 @@ class ViewController: UIViewController {
         } else {
             return "Bill Gates"
         }
+    }
+    
+    func getRandomFact() -> (String, String) {
+        randomPerson()
+//        for (index, value) in billAndSteveFacts {
+//            return String(randomIndex(fromArray: [(index)])); (value)
+//        }
+        return ("Bill Gates", "Steve Jobs")
+    }
+    
+    func showFact() {
+        var correctPerson: String = ""
+        
+    
+        if getRandomFact() == ("Bill Gates", "1") {
+            correctPerson = "Bill Gates"
+            factCounter.text = "1/9"
+            steveBtn.isHidden = true
+            steveLabel.isHidden = true
+        } else {
+            correctPerson = "Steve Jobs"
+            factCounter.text = "1/9"
+            billBtn.isHidden = true
+            billLabel.isHidden = true
+        }
+        
+    }
+    
+    @IBAction func steveBtnPressed(_ sender: UIButton) {
+        getRandomFact()
+        
+        showFact()
+    }
+    
+    @IBAction func billBtnPressed(_ sender: UIButton) {
+        getRandomFact()
+        
+        showFact()
+    }
+    @IBAction func nextFact(_ sender: UIButton) {
+        nextQuestion()
+    }
+    
+    func nextQuestion() {
+        steveLabel.isHidden = false
+        steveBtn.isHidden = false
+        billLabel.isHidden = false
+        billBtn.isHidden = false
     }
     
 }
