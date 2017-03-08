2c96a3611732a4373347dff6ecbe6d5779a5045c
willsputra <suryadiputrawilliam@gmail.com>
2017-03-08 23:47:54 +0800

    Working v1

--- BillOrSteve/ViewController.swift
+++ BillOrSteve/ViewController.swift
@@ -11,10 +11,61 @@ import UIKit
 class ViewController: UIViewController {
     
     // Create your stored properties here
+    var billAndSteveFacts: [String : [String]] = [ : ]
+    var correctPerson: String = ""
     
+    func createFacts() {
+        billAndSteveFacts["Bill Gates"] = [
+            "He aimed to become a millionaire by the age of 30. However, he became a billionaire at 31.",
+        "He scored 1590 (out of 1600) on his SATs.",
+        "His foundation spends more on global health each year than the United Nation's World Health Organization.",
+        "The private school he attended as a child was one of the only schools in the US with a computer. The first program he ever used was a tic-tac-toe game.",
+        "In 1994, he was asked by a TV interviewer if he could jump over a chair from a standing position. He promptly took the challenge and leapt over the chair like a boss."]
+        
+        billAndSteveFacts["Steve Jobs"] = [
+            "He took a calligraphy course, which he says was instrumental in the future company products' attention to typography and font.",
+            "Shortly after being shooed out of his company, he applied to fly on the Space Shuttle as a civilian astronaut (he was rejected) and even considered starting a computer company in the Soviet Union.",
+            "He actually served as a mentor for Google founders Sergey Brin and Larry Page, even sharing some of his advisers with the Google duo.",
+            "He was a pescetarian, meaning he ate no meat except for fish."]
+    }
     
+    var score = 0
+    
+    func showFact() {
+        let showRandomFact = getRandomFact()
+        factLabel.text = "\(showRandomFact.fact)"
+        correctPerson = showRandomFact.name
+    }
+
+    @IBOutlet weak var factLabel: UILabel!
+    @IBOutlet weak var scoreCounter: UILabel!
+    @IBOutlet weak var steveOutlet: UIButton!
+    @IBOutlet weak var billOutlet: UIButton!
+    @IBAction func steveButton(_ sender: Any) {
+        let buttonName = "Steve Jobs"
+        if buttonName == correctPerson {
+            score+=1
+            scoreCounter.text = "\(score)"
+            showFact()
+        } else {
+            showFact()
+        }
+    }
+    
+    @IBAction func billButton(_ sender: Any) {
+        let buttonName = "Bill Gates"
+        if buttonName == correctPerson {
+            score+=1
+            scoreCounter.text = "\(score)"
+            showFact()
+        } else {
+            showFact()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        createFacts()
+        showFact()
     }
     
     
@@ -34,6 +85,16 @@ class ViewController: UIViewController {
         } else {
             return "Bill Gates"
         }
+    }
+    
+    func getRandomFact() -> (name: String, fact: String) {
+        let randomName = randomPerson()
+        var randomFact = ""
+        //let randomFactOptional = billAndSteveFacts[randomName]
+        if let randomFactArray = billAndSteveFacts[randomName] {
+            randomFact = randomFactArray[randomIndex(fromArray: randomFactArray)]
+        }
+        return (name: randomName, fact: randomFact)
     }
     
 }
