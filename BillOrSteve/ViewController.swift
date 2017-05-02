768158d09398e57ba8bfbc5848da2b1086a8d17f
Joseph Turk <joseph.r.turk@gmail.com>
2017-05-02 12:02:14 -0500

    Finish implementing Bill and Steve game

--- BillOrSteve/ViewController.swift
+++ BillOrSteve/ViewController.swift
@@ -10,15 +10,110 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // Create your stored properties here
+    @IBOutlet weak var factlabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var askButton: UIButton!
+    
+    
+    var billAndSteveFacts: [String : [String]] = [:]
+    var factPerson = ""
+    var questionsAsked = 0
+    var score = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        askButton.isHidden = true
+        createFacts()
+        displayRandomFact(fact: getRandomFact())
+    }
+    
+    func createFacts() {
+        let steveFacts = [
+            "He took a calligraphy course, which he says was instrumental in the future company products' attention to typography and font.",
+            "Shortly after being shooed out of his company, he applied to fly on the Space Shuttle as a civilian astronaut (he was rejected) and even considered starting a computer company in the Soviet Union.",
+            "He actually served as a mentor for Google founders Sergey Brin and Larry Page, even sharing some of his advisers with the Google duo.",
+            "He was a pescetarian, meaning he ate no meat except for fish."
+        ]
+        let billFacts = [
+            "He aimed to become a millionaire by the age of 30. However, he became a billionaire at 31.",
+            "He scored 1590 (out of 1600) on his SATs.",
+            "His foundation spends more on global health each year than the United Nation's World Health Organization.",
+            "The private school he attended as a child was one of the only schools in the US with a computer. The first program he ever used was a tic-tac-toe game.",
+            "In 1994, he was asked by a TV interviewer if he could jump over a chair from a standing position. He promptly took the challenge and leapt over the chair like a boss."
+        ]
+        
+        billAndSteveFacts["Bill Gates"] = billFacts
+        billAndSteveFacts["Steve Jobs"] = steveFacts
+    }
+    
+    func getRandomFact() -> (String, String) {
+        let factPerson = randomPerson()
+        var randomFact = ""
+        if let personFacts = billAndSteveFacts[factPerson] {
+            let indexOfFact = randomIndex(fromArray: personFacts)
+            randomFact = personFacts[indexOfFact]
+            billAndSteveFacts[factPerson]?.remove(at: indexOfFact)
+        }
+        
+        questionsAsked += 1
+        
+        return (randomFact, factPerson)
+    }
+    
+    func displayRandomFact(fact: (question: String, answer: String)) {
+        factlabel.text = fact.question
+        factPerson = fact.answer
+        askButton.isHidden = true
+    }
+    
+    @IBAction func steveButton(_ sender: Any) {
+        if factPerson == "Steve Jobs" {
+            score += 1
+            scoreLabel.text = String(score)
+            
+            if questionsAsked < 5 {
+                factlabel.text = "Correct!"
+                askButton.isHidden = false
+            } else {
+                factlabel.text = "Correct! You got \(score) out of 5 correct."
+            }
+        } else {
+            if questionsAsked < 5 {
+                factlabel.text = "Wrong!"
+                askButton.isHidden = false
+            } else {
+                factlabel.text = "Wrong! You got \(score) out of 5 correct."
+            }
+            
+        }
+    }
+    
+    @IBAction func billButton(_ sender: Any) {
+        if factPerson == "Bill Gates" {
+            score += 1
+            scoreLabel.text = String(score)
+            
+            if questionsAsked < 5 {
+                factlabel.text = "Correct!"
+                askButton.isHidden = false
+            } else {
+                factlabel.text = "Correct! You got \(score) out of 5 correct."
+            }
+        } else {
+            if questionsAsked < 5 {
+                factlabel.text = "Wrong!"
+                askButton.isHidden = false
+            } else {
+                factlabel.text = "Wrong! You got \(score) out of 5 correct."
+            }
+        }
     }
     
     
-    
+    @IBAction func askButton(_ sender: Any) {
+        displayRandomFact(fact: getRandomFact())
+    }
     
     
     // Helper Functions
---
+++ InitialViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InitialViewController.swift
+//  BillOrSteve
+//
+//  Created by Joseph Turk on 5/2/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startPlayingButton(_ sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
