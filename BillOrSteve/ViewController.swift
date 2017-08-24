bbaa017da82c81897be5a5cdec1134c564244571
dryng <dryngler@gmail.com>
2017-08-23 17:57:53 -0700

    donne

--- BillOrSteve/ViewController.swift
+++ BillOrSteve/ViewController.swift
@@ -10,15 +10,96 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // Create your stored properties here
+    @IBOutlet weak var factLabel: UILabel!
     
+    @IBOutlet weak var scoreCounter: UILabel!
+    
+    @IBOutlet weak var steveButton: UILabel!
+    
+    @IBOutlet weak var billButton: UILabel!
+    
+    var billAndSteveFacts: [String : [String]] = [:]
+    var correctPerson: String = ""
+    var counter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createFacts()
+        showFact()
     }
     
+    func showFact() {
+        let (person, fact) = getRandomFact()
+        correctPerson = person
+        factLabel.text = fact
+    }
     
+    @IBAction func stevePicture(_ sender: Any) {
+        if correctPerson == "Steve Jobs" {
+            counter += 1
+            scoreCounter.text = String(counter)
+        }
+        showFact()
+    }
     
+        @IBAction func billPicture(_ sender: Any) {
+            if correctPerson == "Bill Gates" {
+                counter += 1
+                scoreCounter.text = String(counter)
+            }
+            showFact()
+        }
+
+    
+    func getRandomFact() -> (String, String) {
+        let person = randomPerson()
+        
+        if let facts = billAndSteveFacts[person] {
+            let fact = produceFact(from: facts, with: person)
+            return (person, fact)
+        }
+        
+        let otherperson = person == "Bill Gates" ? "Steve Jobs" : "Bill Gates"
+        
+        if let facts = billAndSteveFacts[otherperson] {
+            let fact = produceFact(from: facts, with: otherperson)
+            return (otherperson, fact)
+    }
+        
+    billButton.isUserInteractionEnabled = false
+    steveButton.isUserInteractionEnabled = false
+        
+        return ("", "GAME OVER")
+        
+    }
+    
+    func produceFact(from array: [String], with person: String) -> String {
+        var copy = array
+        let index = randomIndex(fromArray: copy)
+        let fact = copy[index]
+        copy.remove(at: index)
+        billAndSteveFacts[person] = copy.isEmpty ? nil : copy
+        return fact 
+    }
+
+    func createFacts() {
+        let billFacts = [
+            "He aimed to become a millionaire by the age of 30. However, he became a billionaire at 31.",
+            "He scored 1590 (out of 1600) on his SATs.", "His foundation spends more on global health each year than the United Nation's World Health Organization.",
+            "The private school he attended as a child was one of the only schools in the US with a computer. The first program he ever used was a tic-tac-toe game.",
+            "In 1994, he was asked by a TV interviewer if he could jump over a chair from a standing position. He promptly took the challenge and leapt over the chair like a boss."
+        ]
+        
+        let steveFacts = [
+            "He took a calligraphy course, which he says was instrumental in the future company products' attention to typography and font.",
+            "Shortly after being shooed out of his company, he applied to fly on the Space Shuttle as a civilian astronaut (he was rejected) and even considered starting a computer company in the Soviet Union.",
+            "He actually served as a mentor for Google founders Sergey Brin and Larry Page, even sharing some of his advisers with the Google duo.",
+            "He was a pescetarian, meaning he ate no meat except for fish."
+        ]
+        
+        billAndSteveFacts["Bill Gates"] = billFacts
+        billAndSteveFacts["Steve Jobs"] = steveFacts
+    }
     
     
     // Helper Functions
@@ -37,3 +118,7 @@ class ViewController: UIViewController {
     }
     
 }
+
+
+
+
