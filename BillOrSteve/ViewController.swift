e68e12d59bacbccc44343089e56f9f1a835f62a6
shanetreynolds6 <shanetreynolds@icloud.com>
2017-12-07 20:37:40 -0500

    billAndSteve Lab

--- BillOrSteve/ViewController.swift
+++ BillOrSteve/ViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     
     // Create your stored properties here
+    var facts: [String: [String]] = [:]
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createFacts()
     }
     
     
@@ -35,5 +39,14 @@ class ViewController: UIViewController {
             return "Bill Gates"
         }
     }
-    
+    func createFacts() {
+        let billFacts = ["He aimed to become a millionaire by the age of 30. However, he became a billionaire at 31.", "He scored 1590 (out of 1600) on his SATs.", "His foundation spends more on global health each year than the United Nation's World Health Organization."]
+        let steveFacts = ["He was a pescetarian, meaning he ate no meat except for fish.", "He actually served as a mentor for Google founders Sergey Brin and Larry Page, even sharing some of his advisers with the Google duo.", "He took a calligraphy course, which he says was instrumental in the future company products' attention to typography and font."]
+        facts["Bill Gates"] = billFacts
+        facts["Steve Jobs"] = steveFacts
+        
+    }
+    func getRandomFact() -> (String, String) {
+        randomPerson()
+    }
 }
